221c50009f24b2bd252fc998548b352bab08a87b
as0004409 <as0004409>
2021-08-18 17:54:16 +0300

    First homework

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import s from './Message.module.css'
+
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={s.rootMess}>
+            <img src={props.avatar}/>
+            <div className={s.messInfo}>
+                <div>{props.name}</div>
+                <div>{props.message}</div>
+                <div className={s.timeMess}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
