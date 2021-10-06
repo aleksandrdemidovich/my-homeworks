c37466237e5259b55929cc56e42ce2eb876d7ae8
Aleksandr Demidovich <demid.kbr@mail.ru>
2021-10-06 15:12:07 +0300

    Added solution 8 tasks

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -2,7 +2,11 @@ import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
-// export type UserType =
+export type UserType = {
+    _id: number
+    name: string
+    age: number
+}
 
 const initialPeople = [
     {_id: 0, name: 'Кот', age: 3},
@@ -17,13 +21,16 @@ function HW8() {
     const [people, setPeople] = useState<any>(initialPeople) // need to fix any
 
     // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+    const finalPeople = people.map((p: UserType) => (
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '340px'}}
+             key={p._id}>
+            <span>{p.name}</span><span>{p.age}</span>
         </div>
     ))
 
     const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const check = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
 
     return (
         <div>
@@ -32,15 +39,18 @@ function HW8() {
 
             {/*should work (должно работать)*/}
             {finalPeople}
+            < div style={{display: 'flex', flexDirection: 'row'}}>
+                <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+                <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
+                <div><SuperButton onClick={check}>check 18+</SuperButton></div>
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+                {/*<div>sort down</div>*/}
+                {/*check 18*/}
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
+
+                {/*для личного творчества, могу проверить*/}
+                {/*<AlternativePeople/>*/}
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,20 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: Array<UserType>, action: {type: string, payload: string | number }): any => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+           if(action.payload === 'up') {
+               return [...state.sort((a, b) => a.name > b.name ? 1 : -1)];
+           }else if(action.payload === 'down'){
+               return [...state.sort((a, b) => a.name < b.name ? 1 : -1)];
+            }
+           return state
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(u => u.age > action.payload);
         }
         default: return state
     }
 }
+
+
