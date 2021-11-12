5146182954481ac51d065cf2f8869ea7b451bbf0
Aleksandr Demidovich <demid.kbr@mail.ru>
2021-11-12 16:09:37 +0300

    refactor home task 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -4,25 +4,27 @@ import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
-    const [value, setValue] = React.useState<number[]>([value1, 100]);
+    const [value2, setValue2] = useState(0)
 
     return (
         <div>
             <hr/>
             homeworks 11
-            <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
-                <span style={{position:'absolute'}}>{value1}</span>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <span style={{position: 'absolute'}}>{value1}</span>
                 <SuperRange onChangeRange={setValue1}
                             value={value1}
                 />
             </div>
 
-            <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
-                <span style={{position:'absolute'}}>{value1}</span>
-                <SuperDoubleRange value={[value1, value[1]]}
-                                  onChangeRange={(value) => {setValue(value)}}
-                />
-                <span style={{paddingLeft:'10px'}}>{value[1]}</span>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <span style={{position: 'absolute'}}>{value1}</span>
+                <SuperDoubleRange value={[value1, value2]}
+                                  onChangeRange={(value) => {
+                                      setValue1(value[0]);
+                                      setValue2(value[1])
+                                  }}/>
+                <span style={{paddingLeft: '10px'}}>{value2}</span>
             </div>
 
             <hr/>
